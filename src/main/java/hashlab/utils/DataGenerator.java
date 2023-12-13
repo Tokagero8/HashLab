5448050986d65a661315e2f4cb53fbb7a3713e72
Tokagero8 <emilczyzewski@wp.pl>
2023-12-13 21:29:57 +0100

    Multiple generation added.

--- src/main/java/hashlab/utils/DataGenerator.java
+++ src/main/java/hashlab/utils/DataGenerator.java
@@ -5,7 +5,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class DataGenerator {
 
-    public String generateUniformString(int length){
+    /*
+    public static String generateUniformString(int length){
         int leftLimit = 97;
         int rightLimit = 122;
         Random random = new Random();
@@ -14,17 +15,41 @@ public class DataGenerator {
                 .limit(length)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
-    }
+    }*/
 
-    public double generateUniformValue(double min, double max){
+    public static double generateUniformValue(double min, double max){
         return ThreadLocalRandom.current().nextDouble(min, max);
     }
 
-    public double generateGaussianValue(double mean, double deviation){
+    public static double[] generateUniformValues(double min, double max, int amount) {
+        double[] values = new double[amount];
+        for (int i = 0; i < amount; i++) {
+            values[i] = ThreadLocalRandom.current().nextDouble(min, max);
+        }
+        return values;
+    }
+
+    public static double generateGaussianValue(double mean, double deviation){
         return ThreadLocalRandom.current().nextGaussian() * deviation + mean;
     }
 
-    public double generateExponentialValue(double lambda){
+    public static double[] generateGaussianValues(double mean, double deviation, int amount) {
+        double[] values = new double[amount];
+        for (int i = 0; i < amount; i++) {
+            values[i] = ThreadLocalRandom.current().nextGaussian() * deviation + mean;
+        }
+        return values;
+    }
+
+    public static double generateExponentialValue(double lambda){
         return Math.log(1 - ThreadLocalRandom.current().nextDouble()) / (-lambda);
+    }
+
+    public static double[] generateExponentialValues(double lambda, int amount) {
+        double[] values = new double[amount];
+        for (int i = 0; i < amount; i++) {
+            values[i] = Math.log(1 - ThreadLocalRandom.current().nextDouble()) / (-lambda);
+        }
+        return values;
     }
 }
